2bc4c445457254e5fe26c785f3d9c0c5dbee312f
Tbyrne23 <81589289+Tbyrne23@users.noreply.github.com>
2023-05-06 20:44:46 -0400

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,27 @@
+var tasks = [
+    "Do laundry",
+    "Clean the kitchen",
+    "Call Mom",
+    "Take out the trash",
+    "Go grocery shopping"
+  ];
+  
+  // Function to add a random task to the to-do list
+  function addTask() {
+    // Get a random task from the array
+    var randomIndex = Math.floor(Math.random() * tasks.length);
+    var task = tasks[randomIndex];
+  
+    // Create a new list item element
+    var li = document.createElement("li");
+    li.textContent = task;
+  
+    // Add the new list item to the to-do list
+    var ul = document.getElementById("todolist");
+    ul.appendChild(li);
+  }
+
+  function removeTask(){
+    var ul = document.getElementById("todolist");
+    ul.remove()
+  }
